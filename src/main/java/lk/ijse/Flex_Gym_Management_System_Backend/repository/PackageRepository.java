cd9a18ec9e000a0493ad1c7e8cef4fd122146073
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-07-31 02:52:56 +0530

    Package Repo Completely Done!

---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/PackageRepository.java
@@ -0,0 +1,14 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.repository;
+
+import lk.ijse.Flex_Gym_Management_System_Backend.entity.Package;
+import lk.ijse.Flex_Gym_Management_System_Backend.enumeration.PackageStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PackageRepository extends JpaRepository<Package,Long> {
+    List<Package> findAllByPackageStatus(PackageStatus status);
+    Optional<Package> findByPackageNameAndPackageStatus(String packageName, PackageStatus status);
+}
