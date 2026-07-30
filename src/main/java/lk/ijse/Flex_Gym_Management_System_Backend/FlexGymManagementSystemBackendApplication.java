e20705851735f0838f4b4ca28d5598d5dcbe93d5
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-07-30 22:53:58 +0530

    First Commited!

---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/FlexGymManagementSystemBackendApplication.java
@@ -0,0 +1,11 @@
+package lk.ijse.Flex_Gym_Management_System_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexGymManagementSystemBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FlexGymManagementSystemBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/lk/ijse/Flex_Gym_Management_System_Backend/FlexGymManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.Flex_Gym_Management_System_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexGymManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
