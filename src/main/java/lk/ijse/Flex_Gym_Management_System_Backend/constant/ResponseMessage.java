a6bd7bb7ec2388894e97d9fb2b3c7c0c0553464a
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-07-31 02:48:47 +0530

    Response Files Added!

---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/constant/ResponseMessage.java
@@ -0,0 +1,6 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.constant;
+
+public class ResponseMessage {
+    public static String SUCCESS_MESSAGE = "Operation Successful...";
+    public static String ERROR_MESSAGE = "Operation Failed...";
+}
---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/constant/ResponseStatusCode.java
@@ -0,0 +1,6 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.constant;
+
+public class ResponseStatusCode {
+    public static int  OPERATION_SUCCESS = 0;
+    public static int  OPERATION_FAIL = 1;
+}
