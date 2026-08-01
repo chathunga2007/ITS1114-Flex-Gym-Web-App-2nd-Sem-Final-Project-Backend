780601b02acf14bddff16c2ba551a2338c3f7af6
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-02 00:45:28 +0530

    Add AttendanceScanDTO for member ID QR scanning!

---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/dto/AttendanceScanDTO.java
@@ -0,0 +1,12 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AttendanceScanDTO {
+    private Long memberId;
+}
