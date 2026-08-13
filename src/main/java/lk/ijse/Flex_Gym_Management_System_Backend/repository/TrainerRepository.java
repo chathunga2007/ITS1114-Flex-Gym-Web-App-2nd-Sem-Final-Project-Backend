0f3151974611861fe7466b68335505ba85446fa2
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-14 00:09:15 +0530

    add trainer repository for database operations!

---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import lk.ijse.Flex_Gym_Management_System_Backend.entity.Trainer;
+
+@Repository
+public interface TrainerRepository extends JpaRepository<Trainer, Long> {
+}
