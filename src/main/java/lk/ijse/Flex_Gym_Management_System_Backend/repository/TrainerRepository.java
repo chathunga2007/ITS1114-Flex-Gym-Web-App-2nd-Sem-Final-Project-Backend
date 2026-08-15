3b8ece18863d9526a031f3316b179647c14b251b
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-15 23:07:21 +0530

    trainer repository updated!

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/TrainerRepository.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/TrainerRepository.java
@@ -1,9 +1,12 @@
 package lk.ijse.Flex_Gym_Management_System_Backend.repository;
 
+import lk.ijse.Flex_Gym_Management_System_Backend.enumeration.TrainerStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import lk.ijse.Flex_Gym_Management_System_Backend.entity.Trainer;
+import java.util.List;
 
 @Repository
 public interface TrainerRepository extends JpaRepository<Trainer, Long> {
+    List<Trainer> findAllByStatus(TrainerStatus status);
 }
