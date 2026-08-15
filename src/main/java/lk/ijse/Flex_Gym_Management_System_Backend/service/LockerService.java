334c0e75a51605567af6d299333c0db506dffe81
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-15 23:31:04 +0530

    implement locker service and business logic!

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/service/LockerService.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/service/LockerService.java
@@ -2,7 +2,7 @@ package lk.ijse.Flex_Gym_Management_System_Backend.service;
 
 import lk.ijse.Flex_Gym_Management_System_Backend.dto.LockerDTO;
 import java.util.List;
-  
+
 public interface LockerService {
     LockerDTO saveLocker(LockerDTO lockerDTO);
     LockerDTO updateLocker(LockerDTO lockerDTO);
