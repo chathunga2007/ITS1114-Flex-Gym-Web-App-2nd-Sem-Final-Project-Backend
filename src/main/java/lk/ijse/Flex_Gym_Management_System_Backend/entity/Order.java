1b69a819c35a46032c5a209d2d472b429c398abf
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-21 13:29:47 +0530

    order & orderItem entity added!

---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/entity/Order.java
@@ -0,0 +1,35 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.entity;
+
+import jakarta.persistence.*;
+import lk.ijse.Flex_Gym_Management_System_Backend.enumeration.OrderStatus;
+import lk.ijse.Flex_Gym_Management_System_Backend.enumeration.PaymentStatus;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private BigDecimal totalAmount;
+    private LocalDateTime orderDate = LocalDateTime.now();
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus = PaymentStatus.PAID;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+}
---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/entity/OrderItem.java
@@ -0,0 +1,27 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
