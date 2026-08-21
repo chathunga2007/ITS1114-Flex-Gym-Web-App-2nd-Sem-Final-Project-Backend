bf5be8739891746fcf78f5ab32ff1c1afe1ebbe9
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-21 12:52:37 +0530

    add workout plan enum classes!

---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/enumeration/PlanDifficulty.java
@@ -0,0 +1,7 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.enumeration;
+
+public enum PlanDifficulty {
+    BEGINNER,
+    INTERMEDIATE,
+    ADVANCED
+}
---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/enumeration/PlanStatus.java
@@ -0,0 +1,8 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.enumeration;
+
+public enum PlanStatus {
+    ACTIVE,
+    COMPLETED,
+    CANCELLED,
+    DELETED
+}
