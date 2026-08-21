3fd5cd034be84dfe6e3a5b252b59f7d534ef50b4
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-21 14:16:41 +0530

    trainer role added!

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/enumeration/UserRole.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/enumeration/UserRole.java
@@ -3,5 +3,6 @@ package lk.ijse.Flex_Gym_Management_System_Backend.enumeration;
 public enum UserRole {
     ROLE_ADMIN,
     ROLE_RECEPTIONIST,
-    ROLE_MEMBER
+    ROLE_MEMBER,
+    ROLE_TRAINER,
 }
