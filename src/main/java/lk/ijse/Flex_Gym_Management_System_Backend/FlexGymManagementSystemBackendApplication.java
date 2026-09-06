23f7c2a452e23865f0e68dfffdc20f5a5bbc26f4
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-09-07 01:00:53 +0530

    Initial Commited!

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/FlexGymManagementSystemBackendApplication.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/FlexGymManagementSystemBackendApplication.java
@@ -2,8 +2,10 @@ package lk.ijse.Flex_Gym_Management_System_Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FlexGymManagementSystemBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlexGymManagementSystemBackendApplication.class, args);
