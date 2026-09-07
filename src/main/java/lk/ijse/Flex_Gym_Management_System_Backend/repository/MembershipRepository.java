c44614ea7b2f40cbd5b8929124fcc8e1f400c8ea
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-09-08 03:03:29 +0530

    Membership Repo Updated!

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/MembershipRepository.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/MembershipRepository.java
@@ -9,8 +9,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MembershipRepository extends JpaRepository<Membership,Long> {
+public interface MembershipRepository extends JpaRepository<Membership, Long> {
     Optional<Membership> findByMemberAndMembershipStatus(Member member, MembershipStatus status);
+    Optional<Membership> findByMember(Member member);
     List<Membership> findAllByMember(Member member);
     List<Membership> findAllByMembershipStatus(MembershipStatus status);
+    List<Membership> findAllByMembershipStatusIn(List<MembershipStatus> statuses);
 }
