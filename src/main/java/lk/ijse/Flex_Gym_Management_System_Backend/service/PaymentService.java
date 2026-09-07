9c5d8b8700f199ce5303e146e05b693f96bd78f3
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-09-08 03:05:03 +0530

    Payment Service Updated!

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/service/PaymentService.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/service/PaymentService.java
@@ -1,10 +1,12 @@
 package lk.ijse.Flex_Gym_Management_System_Backend.service;
 
 import lk.ijse.Flex_Gym_Management_System_Backend.dto.PaymentDTO;
+import lk.ijse.Flex_Gym_Management_System_Backend.enumeration.PaymentStatus;
 import java.util.List;
 
 public interface PaymentService {
     PaymentDTO savePayment(PaymentDTO paymentDTO);
+    PaymentDTO updatePaymentStatus(Long id, PaymentStatus paymentStatus);
     PaymentDTO getPaymentById(Long id);
     List<PaymentDTO> getAllPayments();
     List<PaymentDTO> getPaymentsByMemberId(Long memberId);
