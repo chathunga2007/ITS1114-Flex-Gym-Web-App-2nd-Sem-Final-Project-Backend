4508c52c184eb2f1f0dd4df2bc8bf6d924d84207
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-09-20 19:34:36 +0530

    add password reset and OTP handling methods to UserService interface

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/service/UserService.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/service/UserService.java
@@ -1,6 +1,9 @@
 package lk.ijse.Flex_Gym_Management_System_Backend.service;
 
+import lk.ijse.Flex_Gym_Management_System_Backend.dto.ForgotPasswordRequestDTO;
+import lk.ijse.Flex_Gym_Management_System_Backend.dto.ResetPasswordRequestDTO;
 import lk.ijse.Flex_Gym_Management_System_Backend.dto.UserDTO;
+import lk.ijse.Flex_Gym_Management_System_Backend.dto.VerifyOtpRequestDTO;
 import java.util.List;
 
 public interface UserService {
@@ -10,4 +13,7 @@ public interface UserService {
     List<UserDTO> getAllUsers();
     UserDTO getAllUser(Long userId);
     UserDTO getUserDetails(String email, String password);
+    void sendForgotPasswordOtp(ForgotPasswordRequestDTO request);
+    boolean verifyOtp(VerifyOtpRequestDTO request);
+    void resetPassword(ResetPasswordRequestDTO request);
 }
