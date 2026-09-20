948831da269461e52265968597261dee9c04abd7
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-09-20 19:34:07 +0530

    create ResetPasswordRequestDTO for updating user password

---
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/dto/ResetPasswordRequestDTO.java
@@ -0,0 +1,14 @@
+package lk.ijse.Flex_Gym_Management_System_Backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordRequestDTO {
+    private String email;
+    private String otp;
+    private String newPassword;
+}
