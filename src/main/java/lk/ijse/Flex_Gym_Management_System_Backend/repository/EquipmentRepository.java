145ed5b8a19520df280e58c2d7a7a45e6c1a324b
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-09-23 23:05:44 +0530

    equipment repo updated!

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/EquipmentRepository.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/EquipmentRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 
 @Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
-    List<Equipment> findByConditionStatus(EquipmentStatus conditionStatus);
 }
