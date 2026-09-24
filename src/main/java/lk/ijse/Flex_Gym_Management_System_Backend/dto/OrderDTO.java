392509902d05ff0c036dc39c6a4d29b0808e10cf
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-09-25 01:09:18 +0530

    Update OrderDTO to include courier tracking, shipping address, and payment method details

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/dto/OrderDTO.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/dto/OrderDTO.java
@@ -1,11 +1,13 @@
 package lk.ijse.Flex_Gym_Management_System_Backend.dto;
 
 import lk.ijse.Flex_Gym_Management_System_Backend.enumeration.OrderStatus;
+import lk.ijse.Flex_Gym_Management_System_Backend.enumeration.PaymentMethod;
 import lk.ijse.Flex_Gym_Management_System_Backend.enumeration.PaymentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -20,5 +22,20 @@ public class OrderDTO {
     private LocalDateTime orderDate;
     private OrderStatus orderStatus;
     private PaymentStatus paymentStatus;
+    private PaymentMethod paymentMethod;
+
+    private String shippingAddress;
+    private String deliveryCity;
+    private String postalCode;
+    private String contactPhone;
+    private String orderNotes;
+
+    private String trackingNumber;
+    private String courierName;
+    private LocalDate estimatedDeliveryDate;
+
+    private String cardLast4;
+    private String cardBrand;
+
     private List<OrderItemDTO> items;
 }
