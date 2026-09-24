1edadc17863f50d647d7ebe18a57453a7bf8349f
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-09-25 01:09:26 +0530

    Update OrderRepository with findByTrackingNumber query method

--- src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/OrderRepository.java
+++ src/main/java/lk/ijse/Flex_Gym_Management_System_Backend/repository/OrderRepository.java
@@ -4,9 +4,11 @@ import lk.ijse.Flex_Gym_Management_System_Backend.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
     List<Order> findAllByMember_MemberIdOrderByOrderDateDesc(Long memberId);
     List<Order> findAllByOrderByOrderDateDesc();
+    Optional<Order> findByTrackingNumber(String trackingNumber);
 }
